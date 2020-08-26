350d63a10597b00b505f892654c5ffb612a7125f
Tumusiime Lauben Junior <tumusiimelauben9@gmail.com>
2020-08-27 06:38:22 +0800

    incoming new files
    added detail.html

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
+from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Question
 
@@ -21,12 +23,17 @@ def vote(request, question_id):
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-   # output = ', '.join([q.question_text for q in latest_question_list])
-   template = loader.get_template('polls/index.html')
-   context = {
-       'latest_question_list' : latest_question_list,
-   }
-
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
     return HttpResponse(template.render(context, request))
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk = question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question' : question})
 
 # Leave the rest of the views (detail, results, vote unchanged)
